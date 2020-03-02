eec1ecfb7725d155d28832b7a9c7edd3e7d0dd7f
Ma3dai <mvgrabov@gmail.com>
2020-03-02 22:07:58 +0300

    перенес морской бой в папку Head First Java

---
+++ SeaWar v1/src/DotComBust.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class DotComBust {
+
+  //Объявляем и инициируем переменные, которые нам понадобятся
+  private GameHelper helper = new GameHelper();
+  private ArrayList<DotCom> dotComList = new ArrayList();
+  private int numOfGuesses = 0;
+
+  private void setUpGame(){
+    //Создадить несколько сайтов и присвоим им адреса
+
+  }
+}
